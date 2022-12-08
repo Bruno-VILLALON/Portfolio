d08b06d151592326e5d9dc30dafbd9e7085b16b3
Bruno-VILLALON <brunovillalon@gmail.com>
2022-12-08 12:37:02 +0100

    colors modified

--- js/index.js
+++ js/index.js
@@ -1,10 +1,12 @@
 // locomotivescroll inisialization
+
 const scroll = new LocomotiveScroll({
     el: document.querySelector('[data-scroll-container]'),
     smooth: true,
     tablet: {smooth: true},
     smartphone: {smooth: true}
 })
+
 
 // body color
 
@@ -15,11 +17,13 @@ const contrastColor = "#e09e50";
 
 scroll.on("scroll", () => {
     if (document.querySelector("#header-color-point.is-inview")) {
-        document.body.style.background = contrastColor;
-        document.body.style.color = darkColor;
-    } else if (document.querySelector("#dark-color.is-inview")){
         document.body.style.background = darkColor;
         document.body.style.color = lightColor;
+        console.log("écran d'accueil");
+    } else if (document.querySelector("#skills-color.is-inview")){
+        document.body.style.background = mediumColor;
+        document.body.style.color = darkColor;
+        console.log("section compétences")
     } else {
         document.body.style.background = lightColor;
         document.body.style.color = darkColor;
