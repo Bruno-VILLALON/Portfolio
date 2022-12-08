181724a61b044e06f03527f6d2f095fc289c6a37
Bruno-VILLALON <brunovillalon@gmail.com>
2022-12-08 14:38:13 +0100

    added some links and modified images

--- js/index.js
+++ js/index.js
@@ -16,14 +16,14 @@ const lightColor = "#e8eceb";
 const contrastColor = "#e09e50";
 
 scroll.on("scroll", () => {
-    if (document.querySelector("#header-color-point.is-inview")) {
+    if (document.querySelector("#header-color.is-inview")) {
         document.body.style.background = darkColor;
         document.body.style.color = lightColor;
-        console.log("écran d'accueil");
+        //console.log("écran d'accueil");
     } else if (document.querySelector("#skills-color.is-inview")){
         document.body.style.background = mediumColor;
         document.body.style.color = darkColor;
-        console.log("section compétences")
+        //console.log("section compétences")
     } else {
         document.body.style.background = lightColor;
         document.body.style.color = darkColor;
